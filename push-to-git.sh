19e53aa4abdfda52fbfc0a2f882a0746451cbaee
Pavan-D4 <ec2-user@ip-172-31-21-52.ec2.internal>
2024-09-05 13:34:06 +0000

    Auto-commit: Thu Sep  5 01:34:06 PM UTC 2024

--- push-to-git.sh
+++ push-to-git.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Set Git user name and email for commits (specific to this repository)
+git config user.name "Pavan-D4"
+#git config user.email "your-email@example.com"
+
 # Navigate to your project directory
 cd /home/ec2-user/work/shell-scripting/
 
