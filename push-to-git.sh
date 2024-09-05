525a39c6367055bc7df6366cc79a726bc2baeadd
Pavan-D4 <pavanreddy5698@gmail.com>
2024-09-05 13:38:13 +0000

    Auto-commit: Thu Sep  5 01:38:13 PM UTC 2024

--- push-to-git.sh
+++ push-to-git.sh
@@ -2,7 +2,7 @@
 
 # Set Git user name and email for commits (specific to this repository)
 git config user.name "Pavan-D4"
-#git config user.email "your-email@example.com"
+git config user.email "pavanreddy5698@gmail.com"
 
 # Navigate to your project directory
 cd /home/ec2-user/work/shell-scripting/
@@ -12,3 +12,8 @@ git add .
 
 # Commit the changes with a message containing the current date and time
 git commit -m "Auto-commit: $(date)"
+
+# Push the changes to the remote repository
+git push origin main 
+
+echo "Changes pushed to Git repository!"
