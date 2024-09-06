e267e1c15caf6694bae3e7c2b6eccba362d80762
Pavan-D4 <pavanreddy5698@gmail.com>
2024-09-06 13:51:20 +0000

    Auto-commit: Fri Sep  6 01:51:20 PM UTC 2024

---
+++ scripts/check_file_readable.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#Write a script that checks if a file exists and if it is readable, writable, or executable. The script should output a message for each condition.
+
+file_nm=$1
+
+if [ -z $file_nm ]; then
+   echo "please pass file name syntax : $0 <filename>"
+else
+   if [ -e $file_nm ]; then
+      echo "file exits"
+      
+#pending...
+   fi
+fi
