e8d05ba42ba804938a46cd89e5535eab36fbcc2a
Pavan <68982480+Pavan-D4@users.noreply.github.com>
2024-09-10 13:38:16 +0530

    Update monitor_cpu.sh

--- scripts/monitor_cpu.sh
+++ scripts/monitor_cpu.sh
@@ -7,8 +7,8 @@ CPU_IDLE=$(top -bn1 | grep "Cpu(s)" | awk '{print $8}')
 
 THRESHOLD=50
 
-#if (( $(echo "$CPU_IDLE > $THRESHOLD" | bc -l) )); then
-if [ $CPU_IDLE > THRESHOLD ]; then
+#if [ $CPU_IDLE > THRESHOLD ]; then
+if (( $(echo "$CPU_IDLE < $THRESHOLD" | bc -l) )); then
 	echo "$(date):CPU usage is below $THRESHOLD%. Sending email alert..." >> $LOG_FILE
 
     echo "Alert: CPU usage is below $THRESHOLD%. Current CPU usage: $CPU_IDLE%" | mail -s "CPU Usage Alert" $EMAIL_ID
